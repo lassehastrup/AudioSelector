5e8e11c4e6659c107c47b37fb460406d89dddfbe
Lasse Hastrup <l@hastrup.me>
2025-01-09 00:01:58 +0100

    Added beginning of background windows service implementation

---
+++ AudioSelectorService/ProjectInstaller.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Configuration.Install;
+using System.ServiceProcess;
+
+[RunInstaller(true)]
+public class ProjectInstaller : Installer
+{
+    private ServiceProcessInstaller processInstaller;
+    private ServiceInstaller serviceInstaller;
+
+    public ProjectInstaller()
+    {
+        processInstaller = new ServiceProcessInstaller
+        {
+            Account = ServiceAccount.LocalSystem
+        };
+
+        serviceInstaller = new ServiceInstaller
+        {
+            ServiceName = "AudioSelectorService",
+            StartType = ServiceStartMode.Automatic
+        };
+
+        Installers.Add(processInstaller);
+        Installers.Add(serviceInstaller);
+    }
+}
+
--- wf_AudioSelectorUI/obj/Debug/net9.0-windows/wf_AudioSelectorUI.AssemblyInfo.cs
+++ wf_AudioSelectorUI/obj/Debug/net9.0-windows/wf_AudioSelectorUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("wf_AudioSelectorUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c58383ef540aaf967e44124bae50949513c7f2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c1ab9640943af6617ed9814666b58ffba7bb007")]
 [assembly: System.Reflection.AssemblyProductAttribute("wf_AudioSelectorUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("wf_AudioSelectorUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
